de9e4f986add1efc8cf8ff07f4f982546030c7c6
Xuyang Huang <xuyang.huang@oracle.com>
2023-03-07 17:58:26 -0800

    first commit, setting up..

---
+++ src/main/java/com/xuyang/dudufirst/DuduFirstApplication.java
@@ -0,0 +1,14 @@
+package com.xuyang.dudufirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuduFirstApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(DuduFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xuyang/dudufirst/DuduFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuyang.dudufirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuduFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
